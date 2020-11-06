3fae976bbc9e7d413f97771d8fba08254d7a8bc5
ntwaricliberi <ntwaricliberi@gmail.com>
2020-11-06 08:12:58 +0200

    Set

---
+++ SET/SetDemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SetDemo{
+    public static void main(String[] args) throws Exception{
+//        initialize person objects
+        Person<Integer,String> p1 = new Person<Integer,String>(32, 173, "Ntwari clarance liberiste");
+        Person<Integer,String> p2 = new Person<Integer,String>(32, 173, "Ntwari clarance liberiste");
+
+//        initialize person set
+        Set <Person <Integer, String>> persons = new HashSet<Person<Integer,String>>();
+
+//        add person objects to set
+        persons.add(p1);
+        persons.add(p2);
+
+        for(Person p:persons){
+            System.out.println(p.getName() + " is " + p.getAge() + " years old and has " + p.getHeight() + " cm of height");
+        }
+    }
+}
