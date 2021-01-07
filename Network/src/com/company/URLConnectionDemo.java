ba2b6aef6ba4841b983bede0e6be5c8149c933c1
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-07 15:28:07 +0200

    creating url connection

---
+++ Network/src/com/company/URLConnectionDemo.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLConnectionDemo {
+
+        public static void main(String[] args) throws Exception{
+            URL url = new URL("https://nuxtjs.org/docs/2.x/get-started/installation");
+
+//
+//            System.out.println("host: " + url.getHost() + "" +
+//                    "port: " + url.getPort() + "" +
+//                    "queryString: "+ url.getQuery());
+
+
+            URLConnection urlc = url.openConnection();
+            InputStream input = urlc.getInputStream();
+
+            int c;
+            while( (c = input.read() ) != -1 ){
+                System.out.print((char)c);
+            }
+
+        }
+}
