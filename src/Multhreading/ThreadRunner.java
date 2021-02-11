90e8c2224e19f428d1eebbefae3028a0b135134d
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-11 16:07:04 +0200

    using Runnable method

--- src/Multhreading/ThreadRunner.java
+++ src/Multhreading/ThreadRunner.java
@@ -5,5 +5,7 @@ public class ThreadRunner {
     public static void main(String[] args){
         ExtendThread thread = new ExtendThread(10,"A");
         thread.start();
+        ExtendThread thread2 = new ExtendThread(5,"B");
+        thread2.start();
     }
 }
