3fc58c54684120b1aa8379755f89c338636aa319
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-19 06:05:35 +0200

    running thread

--- src/Multhreading/ThreadRunner.java
+++ src/Multhreading/ThreadRunner.java
@@ -3,9 +3,14 @@ package Multhreading;
 public class ThreadRunner {
 
     public static void main(String[] args){
-        ExtendThread thread = new ExtendThread(40,"A");
-        thread.start();
-        thread = new ExtendThread(40,"B");
-        thread.start();
+//        ExtendThread thread = new ExtendThread(40,"A");
+//        thread.start();
+//        thread = new ExtendThread(40,"B");
+//        thread.start();
+
+        ImplementRunnable thread2 = new ImplementRunnable();
+        thread2.start();
+
+
     }
 }
