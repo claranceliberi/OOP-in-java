b62de0500b36e004aecc55cdd80dd23f577ee4f3
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-19 06:05:27 +0200

    running thread

--- src/Multhreading/ImplementRunnable.java
+++ src/Multhreading/ImplementRunnable.java
@@ -1,8 +1,25 @@
 package Multhreading;
 
+
 public class ImplementRunnable implements Runnable{
+
+    private Thread th;
+
+
+    public Thread getTh() {
+        return th;
+    }
+
+    public void setTh(Thread th) {
+        this.th = th;
+    }
+
     public void run(){
         System.out.println("running in Implement Runnable");
     }
 
+    public void start(){
+        th = new Thread(this,"Test");
+        th.start();
+    }
 }
