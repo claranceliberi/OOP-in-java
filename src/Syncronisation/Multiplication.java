f9957afbae7dcc973b3ba9a29c3c2ac374c54062
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-25 16:19:28 +0200

    making method syncronized

--- src/Syncronisation/Multiplication.java
+++ src/Syncronisation/Multiplication.java
@@ -1,7 +1,7 @@
 package Syncronisation;
 
 public class Multiplication {
-    public void multiply(Long number) throws Exception{
+    synchronized public void multiply(Long number) throws Exception{
         for(int i=0; i <= number; i++){
             System.out.println(number + " * " + i + " = " + i*number);
             Thread.sleep(200);
